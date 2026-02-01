19daa49e825a7a38f809a52aef30c94c262c291f
Apisara Luengnaruemitchai <aln.apisara@gmail.com>
2026-02-01 20:06:46 +0700

    update the channels

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -17,13 +17,16 @@ class EEGPreprocessor:
         self.target_sfreq = target_sfreq
         self.notch_freq = notch_freq
         
+        # 19 channels
         self.selected_channels = [
-            'FC3.', 'FC1.', 'FCz.', 'FC2.', 'FC4.',
+            'Fc3.', 'Fc1.', 'Fcz.', 'Fc2.', 'Fc4.',
             'C5..', 'C3..', 'C1..', 'Cz..', 'C2..', 'C4..', 'C6..',
-            'CP3.', 'CP1.', 'CPz.', 'CP2.', 'CP4.',
+            'Cp3.', 'Cp1.', 'Cpz.', 'Cp2.', 'Cp4.',
             'Fz..', 'Pz..'
         ]
         
+        # Fc5.,Fc3.,Fc1.,Fcz.,Fc2.,Fc4.,Fc6.,C5..,C3..,C1..,Cz..,C2..,C4..,C6..,Cp5.,Cp3.,Cp1.,Cpz.,Cp2.,Cp4.,Cp6.,Fp1.,Fpz.,Fp2.,Af7.,Af3.,Afz.,Af4.,Af8.,F7..,F5..,F3..,F1..,Fz..,F2..,F4..,F6..,F8..,Ft7.,Ft8.,T7..,T8..,T9..,T10.,Tp7.,Tp8.,P7..,P5..,P3..,P1..,Pz..,P2..,P4..,P6..,P8..,Po7.,Po3.,Poz.,Po4.,Po8.,O1..,Oz..,O2..,Iz..
+
         # Motor imagery runs (left vs right hand)
         self.mi_runs = [4, 8, 12]
         
